d2457da9b7f8c8b1ced2fbc481a392a7da8c7d3c
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-02-22 11:18:01 -0300

    adicionando Projeto Integrador

---
+++ src/main/java/com/generation/colaboratech/ColaboratechApplication.java
@@ -0,0 +1,13 @@
+package com.generation.colaboratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboratechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaboratechApplication.class, args);
+	}
+
+}
