f1dd87e42ea2d841349c442749f4ab478c0162c4
Aurora Marques <auroras321@hotmail.com>
2022-02-25 11:01:33 -0300

    Será?

---
+++ src/main/java/com/generation/colaboratech/controller/TemaController.java
@@ -0,0 +1,12 @@
+package com.generation.colaboratech.controller;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="tb_temas")
+public class TemaController {
+	
+	
+
+}
