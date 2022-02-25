40bf2b6c393fe544e74f5e14babf7036a6a39d94
Aurora Marques <auroras321@hotmail.com>
2022-02-25 11:40:09 -0300

    Adicionando Class TemaController função post

--- src/main/java/com/generation/colaboratech/controller/TemaController.java
+++ src/main/java/com/generation/colaboratech/controller/TemaController.java
@@ -1,12 +1,36 @@
 package com.generation.colaboratech.controller;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.validation.Valid;
 
-@Entity
-@Table(name ="tb_temas")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.colaboratech.model.Tema;
+import com.generation.colaboratech.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/temas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TemaController {
 	
+	@Autowired
+	private TemaRepository temaRepository;
 	
-
+	
+	@PostMapping
+	public ResponseEntity<Tema> postTema(@Valid @RequestBody Tema tema)
+	{
+		if(temaRepository.existsByNomeContainingIgnoreCase(tema.getNome()))
+		{
+			return new ResponseEntity("Este tema já existe.",HttpStatus.BAD_REQUEST);
+		}
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(temaRepository.save(tema));
+	}
 }
