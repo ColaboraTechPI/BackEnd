dea553ab883418a35ee819ee8c175ec7f2359b9a
Luna Ribeiro <lunasantos@alunos.utfpr.edu.br>
2022-03-08 15:14:55 -0300

    Adicionado DeleteUsuario

--- src/main/java/com/generation/colaboratech/controller/UsuarioController.java
+++ src/main/java/com/generation/colaboratech/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,6 +63,16 @@ public class UsuarioController {
 		return usuarioService.atualizarUsuario(usuario)
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<Object> deleteUsuario(@PathVariable Long id){
+		return usuarioRepository.findById(id)
+				.map(resposta -> {
+					usuarioRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
