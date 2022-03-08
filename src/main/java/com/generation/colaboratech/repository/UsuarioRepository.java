b7ab64ff6b8670374365e6d1343f443bdc490171
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-08 14:29:51 -0300

    inserindo as classes usuario Service e Repository

---
+++ src/main/java/com/generation/colaboratech/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.generation.colaboratech.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.colaboratech.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario ,Long> {
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
